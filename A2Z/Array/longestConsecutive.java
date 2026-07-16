f21a7616b4705fc422e97c6cb0335d2ce568cf35
gokul2507 <srigokulrajendiran@gmail.com>
2026-07-16 23:56:27 +0530

    longestConsecutive

---
+++ A2Z/Array/longestConsecutive.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for(int num:nums){
+            set.add(num);
+        }
+        int res=0;
+        for(int num:set){
+            if(!set.contains(num-1)){
+                int count=1;
+                while(set.contains(num+count))count++;
+                res=Math.max(res,count);
+            }
+        }
+        return res;
+    }
+}
